42a6f2e9027f2eccad40f7fa447a230ba9bb29d5
KGr1sha <krylov.grigory@gmail.com>
2023-07-29 19:22:50 +0300

    pistol rotate

---
+++ Assets/Scripts/Main/RotateAim.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAim : MonoBehaviour
+{
+    [SerializeField] private Transform _aitTransform;
+    [SerializeField] private float _pistolAngle;
+
+    private Camera _worldCamera;
+
+    private void Start()
+    {
+        _worldCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        angle = Mathf.Clamp(angle, -_pistolAngle, _pistolAngle);
+        _aitTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 worldPosition = _worldCamera.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0f;
+        return worldPosition;
+    }
+}
