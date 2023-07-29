81c6becf8153f3e00813e5e2cc4293ee1f17c685
KGr1sha <krylov.grigory@gmail.com>
2023-07-29 13:12:52 +0300

    space win and lose

--- Assets/Scripts/SpaceInvaders/PlayerLife.cs
+++ Assets/Scripts/SpaceInvaders/PlayerLife.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField] private GameObject _deathExplosion;
 
+    public static event Action OnPlayerDie;
+
     public void Die()
     {
         Instantiate(_deathExplosion, transform.position, Quaternion.identity);
+        OnPlayerDie?.Invoke();
         Destroy(this.gameObject);
     }
 }
