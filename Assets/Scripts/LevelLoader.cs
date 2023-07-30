9310e65186a2fca8d5b1dbee16fa355f9fa468ce
KGr1sha <krylov.grigory@gmail.com>
2023-07-30 21:09:19 +0300

    shit countdown

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +7,40 @@ public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private Animator _transition;
     [SerializeField] private float _transitionTime = 1f;
+    [SerializeField] private GameObject _countdownObject;
+    [SerializeField] private float _countdownTime;
+
+    private TextMeshProUGUI _textMeshPro;
+
+    private void Start()
+    {
+        _textMeshPro = _countdownObject.GetComponent<TextMeshProUGUI>();
+    }
 
     public void LoadScene(string name)
     {
+        _countdownObject.SetActive(true);
+        if (SceneManager.GetActiveScene().name == "Main")
+        {
+            StartCoroutine(CountDown());
+        }
         StartCoroutine(LoadMinigame(name));
     }
 
     private IEnumerator LoadMinigame(string name)
     {
+        yield return new WaitForSeconds(_countdownTime);
         _transition.SetTrigger("Start");
         yield return new WaitForSeconds(_transitionTime);
         SceneManager.LoadScene(name);
+    }
+
+    private IEnumerator CountDown()
+    {
+        for (int i = 3; i > 0; i--)
+        {
+            _textMeshPro.text = i.ToString();
+            yield return new WaitForSeconds(_countdownTime / 3);
+        }
     }
 }
