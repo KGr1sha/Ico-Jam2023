ed7472869be3d225190425312446f8fdce5aca4a
KGr1sha <krylov.grigory@gmail.com>
2023-07-30 22:20:26 +0300

    minigame trigger fix

--- Assets/Scripts/Main/LoadMinigame1.cs
+++ Assets/Scripts/Main/LoadMinigame1.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class LoadMinigame1 : MonoBehaviour
 {
     [SerializeField] private LevelLoader _levelLoader;
+    [SerializeField] private string _sceneToLoad;
+
+    private bool _activated = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && _activated == false)
         {
-            _levelLoader.LoadScene("Minigame1");
+            _activated = true;
+            _levelLoader.LoadScene(_sceneToLoad);
         }
     }
 }
