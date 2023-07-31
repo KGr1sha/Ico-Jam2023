b4704c5afd3b88519e0d460a7a5039b7648d087a
KGr1sha <krylov.grigory@gmail.com>
2023-07-31 17:55:38 +0300

    minigame sounds

--- Assets/Arcanoid/Scripts/CustomBouncy.cs
+++ Assets/Arcanoid/Scripts/CustomBouncy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CustomBouncy : MonoBehaviour
 {
     BoxCollider2D bc;
+    [SerializeField] private AudioSource _bounceSound;
 
     void Start()
     {
@@ -13,6 +14,7 @@ public class CustomBouncy : MonoBehaviour
     {   
         if (other.gameObject.tag == "Ball")
         {
+            _bounceSound.Play();
             float relativePosition = (other.transform.position.x - transform.position.x) / bc.bounds.size.x;
             other.rigidbody.velocity = new Vector2(relativePosition, 1).normalized * other.rigidbody.velocity.magnitude;
         }
